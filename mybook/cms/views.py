3d34d7a069a341342120e46b1aa84fd14ab2cfee
Ando Yoshikazu <andooown.dev@gmail.com>
2017-10-16 15:48:19 +0900

    書籍情報の編集を行うページを実際にレンダリングするように修正

--- mybook/cms/views.py
+++ mybook/cms/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 from cms.models import Book
+from cms.forms import BookForm
 
 
 def book_list(request):
@@ -16,7 +17,28 @@ def book_list(request):
 
 def book_edit(request, book_id=None):
     """書籍の編集"""
-    return HttpResponse('書籍の編集')
+    if book_id:
+        # book_id が指定されているとき、修正する Book オブジェクトを取得
+        book = get_object_or_404(Book, pk=book_id)
+    else:
+        # book_id が指定されていないとき、新しく追加する Book オブジェクトを作成
+        book = Book()
+
+    if request.method == 'POST':
+        # POST されたデータからフォームを作成
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():  # フォームのバリデーション
+            # Book オブジェクトを保存
+            book = form.save(commit=False)
+            book.save()
+            # 書籍一覧のページにリダイレクト
+            return redirect('cms:book_list')
+    else:
+        # GET のとき、Book インスタンスからフォームを作成
+        form = BookForm(instance=book)
+
+    # 編集ページを出力
+    return render(request, 'cms/book_edit.html', dict(form=form, book_id=book_id))
 
 
 def book_delete(request, book_id):
