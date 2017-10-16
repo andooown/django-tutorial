3084c18ef74f56968c6446cbfef3307d47fb89d6
Ando Yoshikazu <andooown.dev@gmail.com>
2017-10-16 15:33:16 +0900

    書籍一覧のページを実際にレンダリングするように修正

--- mybook/cms/views.py
+++ mybook/cms/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from cms.models import Book
+
 
 def book_list(request):
     """書籍の一覧"""
-    return HttpResponse('書籍の一覧')
+    # 書籍の一覧を取得
+    books = Book.objects.all().order_by('id')
+    # ビューを出力
+    return render(request,
+                  'cms/book_list.html',     # ビューのテンプレート
+                  {'books': books})         # テンプレートに渡すデータ
 
 
 def book_edit(request, book_id=None):
