e52ed99228c9567d95858efca14cac2790cfe26b
Ando Yoshikazu <andooown.dev@gmail.com>
2017-10-18 02:47:49 +0900

    感想の一覧の実装

--- mybook/cms/views.py
+++ mybook/cms/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.views.generic.list import ListView
 
-from cms.models import Book
-from cms.forms import BookForm
+from cms.models import Book, Impression
+from cms.forms import BookForm, ImpressionForm
 
 
 def book_list(request):
@@ -27,7 +28,7 @@ def book_edit(request, book_id=None):
     if request.method == 'POST':
         # POST されたデータからフォームを作成
         form = BookForm(request.POST, instance=book)
-        if form.is_valid():  # フォームのバリデーション
+        if form.is_valid():     # フォームのバリデーション
             # Book オブジェクトを保存
             book = form.save(commit=False)
             book.save()
@@ -48,3 +49,51 @@ def book_delete(request, book_id):
     book.delete()
     # 書籍一覧のページにリダイレクト
     return redirect('cms:book_list')
+
+
+def impression_edit(request, book_id, impression_id=None):
+    """感想の編集"""
+    book = get_object_or_404(Book, pk=book_id)  # 親の書籍を読む
+    if impression_id:
+        # impression_id が指定されているとき、修正する Impression オブジェクトを取得
+        impression = get_object_or_404(Impression, pk=impression_id)
+    else:
+        # impression_id が指定されていないとき、新しく追加する Impression オブジェクトを作成
+        impression = Impression()
+
+    if request.method == 'POST':
+        # POST されたデータからフォームを作成
+        form = ImpressionForm(request.POST, instance=impression)
+        if form.is_valid():     # フォームのバリデーション
+            # Impression オブジェクトを保存
+            impression = form.save(commit=False)
+            impression.book = book  # この感想の、親の書籍をセット
+            impression.save()
+            # 感想一覧のページにリダイレクト
+            return redirect('cms:impression_list', book_id=book_id)
+    else:
+        # GET のとき、Impression インスタンスからフォームを作成
+        form = ImpressionForm(instance=impression)
+
+    # 編集ページを出力
+    return render(request,
+                  'cms/impression_edit.html',                                       # ビューのテンプレート
+                  dict(form=form, book_id=book_id, impression_id=impression_id))    # テンプレートに渡すデータ
+
+
+class ImpressionList(ListView):
+    """感想の一覧"""
+    context_object_name = 'impressions'
+    template_name = 'cms/impression_list.html'
+    paginate_by = 2     # ページングの単位
+
+    def get(self, request, *args, **kwargs):
+        # 親の Book オブジェクトとそれに関連付いた Impression オブジェクトを取得する
+        book = get_object_or_404(Book, pk=kwargs['book_id'])
+        impressions = book.impressions.all().order_by('id')
+        # ListView のアイテムリストに設定
+        self.object_list = impressions
+
+        # ListView をレンダリングして出力
+        context = self.get_context_data(object_list=self.object_list, book=book)
+        return self.render_to_response(context)
